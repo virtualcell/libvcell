26c8cfcbd6999ea60cfc03aebd9c137d5a0b4f2d
jcschaff <schaff.jim@gmail.com>
2025-03-10 15:24:37 -0400

    fix linting and mypy

--- libvcell/_internal/native_calls.py
+++ libvcell/_internal/native_calls.py
@@ -22,34 +22,30 @@ class VCellNativeCalls:
     ) -> ReturnValue:
         try:
             with IsolateManager(self.lib) as isolate_thread:
-                json_ptr: ctypes.c_char_p = self.lib.vcmlToFiniteVolumeInput(
+                json_ptr: bytes = self.lib.vcmlToFiniteVolumeInput(
                     isolate_thread,
                     ctypes.c_char_p(vcml_content.encode("utf-8")),
                     ctypes.c_char_p(simulation_name.encode("utf-8")),
                     ctypes.c_char_p(str(output_dir_path).encode("utf-8")),
                 )
-            if json_ptr is None:
-                raise ValueError("Native function returned null")
             json_str: str = ctypes.cast(json_ptr, ctypes.c_char_p).value.decode("utf-8")
             # self.lib.freeString(json_ptr)
             return ReturnValue.model_validate_json(json_data=json_str)
         except Exception as e:
-            logging.exception(f"Error in vcml_to_finite_volume_input: {e}")
+            logging.exception("Error in vcml_to_finite_volume_input()", exc_info=e)
             raise
 
     def sbml_to_finite_volume_input(self, sbml_content: str, output_dir_path: Path) -> ReturnValue:
         try:
             with IsolateManager(self.lib) as isolate_thread:
-                json_ptr: bytes | None = self.lib.sbmlToFiniteVolumeInput(
+                json_ptr: bytes = self.lib.sbmlToFiniteVolumeInput(
                     isolate_thread,
                     ctypes.c_char_p(sbml_content.encode("utf-8")),
                     ctypes.c_char_p(str(output_dir_path).encode("utf-8")),
                 )
-            if json_ptr is None:
-                raise ValueError("Native function returned null")
             json_str: str = ctypes.cast(json_ptr, ctypes.c_char_p).value.decode("utf-8")
             # self.lib.freeString(json_ptr)
             return ReturnValue.model_validate_json(json_data=json_str)
         except Exception as e:
-            logging.exception(f"Error in sbml_to_finite_volume_input: {e}")
+            logging.exception("Error in sbml_to_finite_volume_input()", exc_info=e)
             raise
