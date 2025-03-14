2d2a514438a9f93b8c39ac8f9af6c6da5b987611
jcschaff <schaff.jim@gmail.com>
2025-03-14 17:38:23 -0400

    removed trying to force env vars in build script.

--- build.py
+++ build.py
@@ -1,4 +1,3 @@
-import os
 import shutil
 import subprocess
 import sys
@@ -6,69 +5,10 @@ from collections.abc import Mapping
 from pathlib import Path
 
 
-def run_command(command: str, cwd: Path, env: Mapping[str, str]) -> None:
+def run_command(command: str, cwd: Path, env: Mapping[str, str] | None = None) -> None:
     result = subprocess.run(command, cwd=cwd, env=env, shell=True, check=True, text=True)
     if result.returncode != 0:
         sys.exit(result.returncode)
-
-
-def check_env() -> Mapping[str, str]:
-    # print the path environment variable
-    PATH: str = os.getenv("PATH", "")
-    LD_LIBRARY_PATH: str = os.getenv("LD_LIBRARY_PATH", "")
-    JAVA_HOME: str | None = os.getenv("JAVA_HOME") or os.getenv("GRAALVM_HOME")
-    GRAALVM_HOME: str | None = os.getenv("GRAALVM_HOME") or os.getenv("JAVA_HOME")
-    MAVEN_HOME: str | None = os.getenv("MAVEN_HOME")
-    if JAVA_HOME is None and GRAALVM_HOME is None:
-        print("JAVA_HOME or GRAALVM_HOME environment variable must be set")
-        sys.exit(1)
-    print(f"PATH={PATH}")
-    print(f"JAVA_HOME={JAVA_HOME}")
-    print(f"GRAALVM_HOME={GRAALVM_HOME}")
-    print(f"MAVEN_HOME={MAVEN_HOME}")
-
-    # find path to mvn
-    mvn_path: str | None = shutil.which("mvn")
-    if mvn_path is None:
-        print("mvn could not be found")
-        sys.exit(1)
-    mvn_install_dir = Path(mvn_path).parent
-
-    # find path to poetry
-    poetry_path: str | None = shutil.which("poetry")
-    if poetry_path is None:
-        print("poetry could not be found")
-        sys.exit(1)
-    poetry_install_dir = Path(poetry_path).parent
-
-    # Check if native-image is installed
-    native_image_path: str | None = shutil.which("native-image")
-    if native_image_path is None:
-        print("native-image could not be found")
-        sys.exit(1)
-    native_image_install_dir = Path(native_image_path).parent
-    native_image_agent_dir = Path(GRAALVM_HOME) / "lib"
-    graalvm_bin_dir = Path(GRAALVM_HOME) / "bin"
-
-    # get PATH and append mvn and poetry install directories
-    NEW_PATH = f"{mvn_install_dir}:{poetry_install_dir}:{native_image_install_dir}:{graalvm_bin_dir}:{PATH}"
-
-    if LD_LIBRARY_PATH == "":
-        LD_LIBRARY_PATH = f"{native_image_agent_dir}"
-    else:
-        LD_LIBRARY_PATH = f"{native_image_agent_dir}/{LD_LIBRARY_PATH}"
-
-    new_env = os.environ.copy()
-    new_env["PATH"] = NEW_PATH
-    new_env["LD_LIBRARY_PATH"] = LD_LIBRARY_PATH
-    if JAVA_HOME is not None:
-        new_env["JAVA_HOME"] = JAVA_HOME
-    if GRAALVM_HOME is not None:
-        new_env["GRAALVM_HOME"] = GRAALVM_HOME
-    if MAVEN_HOME is not None:
-        new_env["MAVEN_HOME"] = MAVEN_HOME
-
-    return new_env
 
 
 def main() -> None:
@@ -80,14 +20,11 @@ def main() -> None:
     # Ensure the libvcell/lib directory exists
     libvcell_lib_dir.mkdir(parents=True, exist_ok=True)
 
-    # Check the environment variables and pass them to the subprocess
-    new_env = check_env()
-
     # Build VCell Java project from submodule
-    run_command("mvn --batch-mode clean install -DskipTests", cwd=vcell_submodule_dir, env=new_env)
+    run_command("mvn --batch-mode clean install -DskipTests", cwd=vcell_submodule_dir)
 
     # Build vcell-native as Java
-    run_command("mvn --batch-mode clean install", cwd=vcell_native_dir, env=new_env)
+    run_command("mvn --batch-mode clean install", cwd=vcell_native_dir)
 
     # Run with native-image-agent to record configuration for native-image
     run_command(
@@ -96,11 +33,10 @@ def main() -> None:
         "src/test/resources/TinySpacialProject_Application0.xml "
         "target/sbml-input",
         cwd=vcell_native_dir,
-        env=new_env,
     )
 
     # Build vcell-native as native shared object library
-    run_command("mvn package -P shared-dll", cwd=vcell_native_dir, env=new_env)
+    run_command("mvn package -P shared-dll", cwd=vcell_native_dir)
 
     # Copy the shared library to libvcell/lib
     for ext in ["so", "dylib", "dll"]:
