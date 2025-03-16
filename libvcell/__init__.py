a44172bc971477c8ea7e1b08affc605fbfd33096
jcschaff <schaff.jim@gmail.com>
2025-03-16 08:44:42 -0400

    python entrypoints return (success: bool, message: str)

---
+++ libvcell/__init__.py
@@ -0,0 +1,6 @@
+from libvcell.solver_utils import vcml_to_finite_volume_input, sbml_to_finite_volume_input
+
+__all__ = [
+    "vcml_to_finite_volume_input",
+    "sbml_to_finite_volume_input"
+]
---
+++ libvcell/solver_utils.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from libvcell._internal.native_calls import ReturnValue, VCellNativeCalls
+
+
+def vcml_to_finite_volume_input(vcml_content: str, simulation_name: str, output_dir_path: Path) -> (bool, str):
+    native = VCellNativeCalls()
+    return_value: ReturnValue = native.vcml_to_finite_volume_input(vcml_content, simulation_name, output_dir_path)
+    return return_value.success, return_value.message
+
+def sbml_to_finite_volume_input(sbml_content: str, output_dir_path: Path) -> (bool, str):
+    native = VCellNativeCalls()
+    return_value: ReturnValue = native.sbml_to_finite_volume_input(sbml_content, output_dir_path)
+    return return_value.success, return_value.message
