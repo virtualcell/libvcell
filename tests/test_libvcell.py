e81f5dd9884f62741334b0d66bc9bb82b5bb2f03
jcschaff <schaff.jim@gmail.com>
2025-04-04 00:48:07 -0400

    create temp directories for sbml/vcell pytests

--- tests/test_libvcell.py
+++ tests/test_libvcell.py
@@ -1,3 +1,4 @@
+import tempfile
 from pathlib import Path
 
 from libvcell import sbml_to_finite_volume_input, sbml_to_vcml, vcml_to_finite_volume_input, vcml_to_sbml, vcml_to_vcml
@@ -21,30 +22,36 @@ def test_sbml_to_finite_volume_input(temp_output_dir: Path, sbml_file_path: Path
     assert msg == "Success"
 
 
-def test_sbml_to_vcml(temp_output_dir: Path, sbml_file_path: Path) -> None:
+def test_sbml_to_vcml(sbml_file_path: Path) -> None:
     sbml_content = sbml_file_path.read_text()
-    vcml_file_path = temp_output_dir / "test.vcml"
-    success, msg = sbml_to_vcml(sbml_content=sbml_content, vcml_file_path=vcml_file_path)
-    assert vcml_file_path.exists()
-    assert success is True
-    assert msg == "Success"
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_output_dir = Path(temp_dir)
+        vcml_file_path = temp_output_dir / "test.vcml"
+        success, msg = sbml_to_vcml(sbml_content=sbml_content, vcml_file_path=vcml_file_path)
+        assert vcml_file_path.exists()
+        assert success is True
+        assert msg == "Success"
 
 
-def test_vcml_to_sbml(temp_output_dir: Path, vcml_file_path: Path, vcml_app_name: str) -> None:
+def test_vcml_to_sbml(vcml_file_path: Path, vcml_app_name: str) -> None:
     vcml_content = vcml_file_path.read_text()
-    sbml_file_path = temp_output_dir / "test.sbml"
-    success, msg = vcml_to_sbml(
-        vcml_content=vcml_content, application_name=vcml_app_name, sbml_file_path=sbml_file_path
-    )
-    assert sbml_file_path.exists()
-    assert success is True
-    assert msg == "Success"
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_output_dir = Path(temp_dir)
+        sbml_file_path = temp_output_dir / "test.sbml"
+        success, msg = vcml_to_sbml(
+            vcml_content=vcml_content, application_name=vcml_app_name, sbml_file_path=sbml_file_path
+        )
+        assert sbml_file_path.exists()
+        assert success is True
+        assert msg == "Success"
 
 
-def test_vcml_to_vcml(temp_output_dir: Path, vcml_file_path: Path) -> None:
+def test_vcml_to_vcml(vcml_file_path: Path) -> None:
     vcml_content = vcml_file_path.read_text()
-    vcml_file_path = temp_output_dir / "test.vcml"
-    success, msg = vcml_to_vcml(vcml_content=vcml_content, vcml_file_path=vcml_file_path)
-    assert vcml_file_path.exists()
-    assert success is True
-    assert msg == "Success"
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_output_dir = Path(temp_dir)
+        vcml_file_path = temp_output_dir / "test.vcml"
+        success, msg = vcml_to_vcml(vcml_content=vcml_content, vcml_file_path=vcml_file_path)
+        assert vcml_file_path.exists()
+        assert success is True
+        assert msg == "Success"
